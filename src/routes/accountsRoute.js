02200336d0b2ef4b1bd3424f9741b4dff0cb27d6
Carolina Lucca <carolinaplucca@gmail.com>
2020-11-14 17:07:30 -0300

    add authentication to account-create endpoint

--- src/routes/accountsRoute.js
+++ src/routes/accountsRoute.js
@@ -1,7 +1,8 @@
 const controller = require('../controllers/accountController');
+const passport = require('passport');
 
 module.exports = app => {
-    app.route('/accounts').get(controller.get);
+    app.route('/accounts').get(passport.authenticate('bearer', { session: false }), controller.get);
     app.route('/accounts/create').post(controller.add);
     app.route('/accounts/remove/:id').delete(controller.remove);
     app.route('/accounts/edit/:id').patch(controller.edit);
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,5 @@ module.exports = {
   accountsRoute: require('./accountsRoute'),
   offersRoute: require('./offersRoute'),
   userRoute: require('./userRoute'),
+  auth: require('../models/auth')
 }
