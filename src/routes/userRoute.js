a9737a0376cb9333933cda5aac1c79bb45790bc3
Carolina Lucca <carolinaplucca@gmail.com>
2020-11-18 17:13:51 -0300

    add local strategy to login route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,8 @@
 const controller = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 module.exports = app => {
-    app.route('/login').post(controller.login);
+    app
+        .route('/login')
+        .post(authController.local, controller.login);
 }
